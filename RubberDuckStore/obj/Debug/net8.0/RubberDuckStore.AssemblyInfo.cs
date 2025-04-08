c77992a0e99985521cc5596dde0a3b72d1c43df9
Jack Click <jack.click@outlook.com>
2025-04-08 17:46:00 +0000

    Added E-Commerce Functionality

--- RubberDuckStore/obj/Debug/net8.0/RubberDuckStore.AssemblyInfo.cs
+++ RubberDuckStore/obj/Debug/net8.0/RubberDuckStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubberDuckStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0c298628ddcc0ded4b46ad9ee6a5d01f647fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7126caccb8c2fa0688329bbf128e0c5fb7c90e82")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubberDuckStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubberDuckStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
